c1cf0bbfa554e2efe02017e07591c84f5a543336
Ramanpreet Chand <rchand595@gmail.com>
2017-04-26 11:55:40 -0400

    setup user and dashboarad components and navbar links

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -13,11 +13,19 @@ import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CalculatorComponent } from './components/calculator/calculator.component';
 import { ConversionComponent } from './components/conversion/conversion.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'calculator', component: CalculatorComponent},
-  {path: 'calculator/conversion', component: ConversionComponent}
+  {path: 'calculator/conversion', component: ConversionComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'dashboard', component: DashboardComponent}
 ]
 
 @NgModule({
@@ -26,7 +34,11 @@ const appRoutes: Routes = [
     HomeComponent,
     NavbarComponent,
     CalculatorComponent,
-    ConversionComponent 
+    ConversionComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    DashboardComponent 
   ],
   imports: [
     BrowserModule,
