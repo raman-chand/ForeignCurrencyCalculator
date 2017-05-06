b583a93e43f6cebda0c399eb797a4fa0574c773e
Ramanpreet Chand <rchand595@gmail.com>
2017-05-06 17:11:04 -0400

    added 30 more currencies

--- client/src/app/components/dashboard/dashboard.component.ts
+++ client/src/app/components/dashboard/dashboard.component.ts
@@ -8,81 +8,163 @@ import { Router } from '@angular/router';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  AFN: number;
+  ARS: number;
   AUD: number;
   BGN: number;
   BRL: number;
   CAD: number;
-  CHF: number;
+  CHF: number; 
   CNY: number;
-  CZK: number;
+  CRC: number;
+  CZK: number; 
   DKK: number;
+  DOP: number;
+  EGP: number;
   EUR: number;
+  FJD: number;
   GBP: number;
-  HKD: number;
+  GTQ: number;
+  GYD: number;
+  HKD: number; 
   HRK: number;
+  HTG: number;
   HUF: number;
+  IDR: number;
   ILS: number;
   INR: number;
-  JPY: number;
+  IRR: number;
+  ISK: number;
+  JMD: number;
+  JPY: number; 
+  KHR: number;
+  KPW: number;
   KRW: number;
-  MXN: number;
-  MYR: number;
-  NOK: number;
+  LRD: number;
+  MOP: number;
+  MNT: number;
+  MXN: number; 
+  MYR: number; 
+  NAD: number;
+  NOK: number; 
   NZD: number;
+  OMR: number;
+  PAB: number;
+  PEN: number;
   PHP: number;
+  PKR: number;
   PLN: number;
+  QAR: number;
   RON: number;
+  RSD: number;
   RUB: number;
+  SAR: number;
   SEK: number;
   SGD: number;
+  SLL: number;
   THB: number;
   TRY: number;
+  TWD: number;
   USD: number;
+  VND: number;
+  XCD: number;
   ZAR: number;
 
   constructor(
   	private authService: AuthService,
   	private router: Router
-  ) {}
+    ) {}
 
   ngOnInit() {
   	this.authService.getLatestRates().subscribe(data => {
-  		// console.log(data.historical);
+      this.AFN = data.AFN;
+      this.ARS = data.ARS;
       this.AUD = data.AUD;
+      
       this.BGN = data.BGN;
       this.BRL = data.BRL;
+
       this.CAD = data.CAD;
       this.CHF = data.CHF;
       this.CNY = data.CNY;
+      this.CRC = data.CRC;
       this.CZK = data.CZK;
+      
       this.DKK = data.DKK;
+      this.DOP = data.DOP;
+
+      this.EGP = data.EGP;
       this.EUR = data.EUR;
+
+      this.FJD = data.FJD;
+
       this.GBP = data.GBP;
+      this.GTQ = data.GTQ;
+      this.GYD = data.GYD;
+
       this.HKD = data.HKD;
       this.HRK = data.HRK;
+      this.HTG = data.HTG;
       this.HUF = data.HUF;
+      
+      this.IDR = data.IDR;
       this.ILS = data.ILS;
       this.INR = data.INR;
+      this.IRR = data.IRR;
+      this.ISK = data.ISK;
+
+      this.JMD = data.JMD;
       this.JPY = data.JPY;
+      
+      this.KHR = data.KHR;
+      this.KPW = data.KPW;
       this.KRW = data.KRW;
+
+      this.LRD = data.LRD;
+      
+      this.MOP = data.MOP;
+      this.MNT = data.MNT;
       this.MXN = data.MXN;
       this.MYR = data.MYR;
+      
+      this.NAD = data.NAD;
       this.NOK = data.NOK;
       this.NZD = data.NZD;
+
+      this.OMR = data.OMR;
+      
+      this.PAB = data.PAB;
+      this.PEN = data.PEN;
       this.PHP = data.PHP;
+      this.PKR = data.PKR;
       this.PLN = data.PLN;
+
+      this.QAR = data.QAR;
+
       this.RON = data.RON;
+      this.RSD = data.RSD;
       this.RUB = data.RUB;
+      
+      this.SAR = data.SAR;
       this.SEK = data.SEK;
       this.SGD = data.SGD;
+      this.SLL = data.SLL;
+      
       this.THB = data.THB;
       this.TRY = data.TRY;
+      this.TWD = data.TWD;
+      
       this.USD = data.USD;
+      
+      this.VND = data.VND;
+
+      this.XCD = data.XCD;
+
       this.ZAR = data.ZAR;
-  	},
-  	err => {
-  		console.log(err);
-  		return false;
-  	});
+    },
+    err => {
+      console.log(err);
+      return false;
+    });
   }
 }
--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -7,48 +7,82 @@ oxr.set({app_id: oxrConfig.key});
 
 module.exports = {
 	latest(req, res, next) {
-		var rates, base, AUD, BGN, BRL, CAD, CHF, CNY, CZK, DKK, EUR, GBP, HKD, HRK, HUF, ILS, INR,
-			JPY, KRW, MXN, MYR, NOK, NZD, PHP, PLN, RON, RUB, SEK, SGD, THB, TRY, USD, ZAR;
+		var rates, base, AFN, ARS, AUD, BGN, BRL, CAD, CHF, CNY, CRC, CZK, DKK, DOP, EGP, EUR, FJD, GBP, GTQ, GYD,
+			HKD, HRK, HTG, HUF, IDR, ILS, INR, IRR, ISK, JMD, JPY, KHR, KPW, KRW, LRD, MOP, MNT, MXN, MYR, NAD, NOK,
+			NZD, OMR, PAB, PEN, PHP, PKR, PLN, QAR, RON, RSD, RUB, SAR, SEK, SGD, SLL, THB, TRY, TWD, USD, VND, XCD, ZAR;
+
 		oxr.latest(function() {
 			rates = oxr.rates;
 			base = oxr.base;
 			console.log(base);
 			console.log(oxr.rates.AUD);
+			AFN=rates.AFN;
+			ARS=rates.ARS;
 			AUD=rates.AUD;
 			BGN=rates.BGN;
 			BRL=rates.BRL;
 			CAD=rates.CAD;
-			CHF=rates.CHF; 
+			CHF=rates.CHF;
 			CNY=rates.CNY;
-		 	CZK=rates.CZK; 
+			CRC=rates.CRC;
+		 	CZK=rates.CZK;
 		 	DKK=rates.DKK;
-		 	EUR=rates.EUR; 
-		 	GBP=rates.GBP; 
+		 	DOP=rates.DOP;
+		 	EGP=rates.EGP;
+		 	EUR=rates.EUR;
+		 	FJD=rates.FJD;
+		 	GBP=rates.GBP;
+		 	GTQ=rates.GTQ;
+		 	GYD=rates.GYD;
 		 	HKD=rates.HKD; 
 		 	HRK=rates.HRK;
+		 	HTG=rates.HTG;
 		 	HUF=rates.HUF;
-		 	ILS=rates.ILS; 
-		 	INR=rates.INR; 
+		 	IDR=rates.IDR;
+		 	ILS=rates.ILS;
+		 	INR=rates.INR;
+		 	IRR=rates.IRR;
+		 	ISK=rates.ISK;
+		 	JMD=rates.JMD;
 		 	JPY=rates.JPY; 
+		 	KHR=rates.KHR;
+		 	KPW=rates.KPW;
 		 	KRW=rates.KRW; 
+		 	LRD=rates.LRD;
+		 	MOP=rates.MOP;
+		 	MNT=rates.MNT;
 		 	MXN=rates.MXN; 
 		 	MYR=rates.MYR; 
+		 	NAD=rates.NAD;
 		 	NOK=rates.NOK; 
-		 	NZD=rates.NZD; 
-		 	PHP=rates.PHP; 
-		 	PLN=rates.PLN; 
-		 	RON=rates.RON; 
-		 	RUB=rates.RUB; 
-		 	SEK=rates.SEK; 
-		 	SGD=rates.SGD; 
+		 	NZD=rates.NZD;
+		 	OMR=rates.OMR;
+		 	PAB=rates.PAB;
+		 	PEN=rates.PEN;
+		 	PHP=rates.PHP;
+		 	PKR=rates.PKR;
+		 	PLN=rates.PLN;
+		 	QAR=rates.QAR;
+		 	RON=rates.RON;
+		 	RSD=rates.RSD;
+		 	RUB=rates.RUB;
+		 	SAR=rates.SAR;
+		 	SEK=rates.SEK;
+		 	SGD=rates.SGD;
+		 	SLL=rates.SLL;
 		 	THB=rates.THB; 
-		 	TRY=rates.TRY; 
-		 	USD=rates.USD; 
+		 	TRY=rates.TRY;
+		 	TWD=rates.TWD;
+		 	USD=rates.USD;
+		 	VND=rates.VND;
+		 	XCD=rates.XCD;
 		 	ZAR=rates.ZAR;		 	
 		 	return res.json({ 
 		 		success: true,
-		 		AUD, BGN, BRL, CAD, CHF, CNY, CZK, DKK, EUR, GBP, HKD, HRK, HUF, ILS, INR,
-				JPY, KRW, MXN, MYR, NOK, NZD, PHP, PLN, RON, RUB, SEK, SGD, THB, TRY, USD, ZAR
+		 		AFN, ARS, AUD, BGN, BRL, CAD, CHF, CNY, CRC, CZK, DKK, DOP, EGP, EUR, FJD, GBP, GTQ, GYD,
+				HKD, HRK, HTG, HUF, IDR, ILS, INR, IRR, ISK, JMD, JPY, KHR, KPW, KRW, LRD, MOP, MNT, MXN, MYR, NAD, NOK,
+				NZD, OMR, PAB, PEN, PHP, PKR, PLN, QAR, RON, RSD, RUB, SAR, SEK, SGD, SLL, THB, TRY, TWD, USD, VND, XCD, 
+				ZAR
 		 	});
 		});
 	},
